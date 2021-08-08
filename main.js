588a301299cc9cc00b4c6b14d9c6588660ab70c3
Kyle Cheung <cheung.kyle1@gmail.com>
2021-08-08 00:12:39 -0400

    finished scaffolding

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const { app, BrowserWindow, contextBridge } = require("electron");
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
-    win.loadFile('index.html')
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  win.loadFile("index.html");
+  //win.webContents.openDevTools();
 }
 
+// on ready launch window
 app.whenReady().then(() => {
-    createWindow();
-})
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// on window close exit app except on macOS
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
